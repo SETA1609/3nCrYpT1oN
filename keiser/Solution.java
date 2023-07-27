d66830143c818ee5996709baa8685972eb505475
SETA1609 <sebastian.stp16@gmail.com>
2023-07-27 22:33:12 +0200

    ft: cypher class was made again, TODO add the WordPlay class and test cases for Cypher

--- keiser/Solution.java
+++ keiser/Solution.java
@@ -5,21 +5,6 @@ import edu.duke.FileResource;
 public class Solution {
 
     public static void main(String[] args) {
-       // FileResource fr = new FileResource();
-       // String input = fr.asString();
-        /*
-         * Cypher c = new Cypher(5, 2, input);
-         * System.out.println(c.getInput());
-         * System.out.println(c.getOutput());
-         * System.out.println(c.getAlpha());
-         * System.out.println(c.getEncrypt1());
-         * System.out.println(c.getEncrypt2());
-         */
-       // CaesarBreaker cb = new CaesarBreaker();
-       // System.out.println(cb.decryptTwoKeys(input));
-       CaesarBreaker cb = new CaesarBreaker();
-       String message = "Xifqvximt tsdtlxzrx iijirvtl ek Uybi afvbw yehvv xyi gfqdse iekmfrrpzdrxzse fj xyi jzich sw tsdtlxrxzseec xifqvxic, fjkie xmmie zr xyi trwk, xyek klv nsipu rvfyeh yj zw xyvvi-hzqvrjmfrrp eeh ulijxzsew lfa xymekj zr xymj nsipu iiceki xf vetl sklvv eii melvvvrkpp xifqvximt. Xrov dsmmek e tzees xyvfyxl e hfsi-wvrqv rru gprremek e jcmxlk-gekl xyek rzfmuw gfpcmjmfrj nmkl sklvv ezvgprrvw ej kaf vbrqgpvw. Zx wyslpu klvvvjfvv esk jyitimji xyek tsdtlxzrx gvftvvkmvw esslx xyiji kvsdikvzg xymekj rru klvmi zrkiietxzse rvv tsdqfr-tceti eeh mdtfvkeex. Nlzpv klzw mj jxzpc r mecmu rvxydiex, ni afych pzov ks edieh xyek dsjx sw klv xifqvximt hyvwkmfrj giftci gfrtiir xyidwvpmij nmkl lrzv ks hf nmkl lfa xymekj rvv tservgkiu. Mk zw mdtfvkeex xyek ymxlnepw eii wljwmtmvrkpp jxiezkyx eeh wdsfxy ks wltgsix xyi himmmek sw wejx grvj, flx eesklvv ijwvrkmrp tisgiixp, aymtl av lwlecpp kebi jfv kieexvh, zw xyek ymxlnepw eii gfrkmeyfyj, mehviu tservgkmek E xf S, eeh rfx nlwk rtgvfbzqrxvpp. Xyi gfviijtfrumek wlfwmvpu fj gfqgykekmfrrp kvsdikvp zw swxvr vvjvviiu ks ej tsdtlxrxzseec ksgscsxc. R xsfh tfvkmfr sw fyi vjwsixj dep si gcejwzjziu ks fvpfrx ks xymj jysjzich eeh eii himmie sc egtcmtekmfrj zr e zrvzikc sw fxyii wmvpuw, klv gvvhzgkmfr sw klv jxiytxlvv fj jfpuiu gvfxvmew eeh xyi vvgfrjxiytxzse fj llqrr sikrrj sizrx kaf. Xyi lrpcqrvb fj slv afvb zw jrwk rpxsimkldw xyek zqgpvqvrk deklvqrxzgrp qfhvpj ks swjvv mewzkyxj zrks eeh eewniiw xf jytl ulijxzsew.";
-       String decrypted = cb.decryptTwoKeys(message);
-       System.out.println(decrypted);
+
     }
 }
