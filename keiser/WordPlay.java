faea4b0126ec8cdcb67b2114ca5558fb62249bbf
SETA1609 <sebastian.stp16@gmail.com>
2023-07-28 11:08:12 +0200

    ft: contructors for decrypt1 and 2 where made in cypher. isWovel was implemented -todo replaceWovels

--- keiser/WordPlay.java
+++ keiser/WordPlay.java
@@ -1,7 +1,26 @@
 package keiser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WordPlay {
 
+    public boolean isVowel(char ch) {
+        boolean isVowel = false;
+        Character[] vowelArray = {'a', 'e', 'i', 'o', 'u'};
+        List<Character> vowels = new ArrayList<Character>(Arrays.asList(vowelArray));
 
+        if (vowels.contains(Character.toLowerCase(ch))) {
+            isVowel = true;
+        }
+
+        return isVowel;
+    }
+
+
+    public String replaceVowels(String phrase, Character ch) {
+        return phrase;
+    }
 
 }
